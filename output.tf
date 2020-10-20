e217f812755a37f0237c49c6d1bea4a274eef82a
Frey Khademi <frey.khademi@gmail.com>
2020-10-20 20:59:45 +0200

    fix out

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "vm" {
+/* output "vm" {
   description = "The created VM as an object with all of it's attributes. This was created using the azurerm_virtual_machine resource."
   value       = azurerm_virtual_machine.instance
 }
@@ -11,4 +11,4 @@ output "pub_ip" {
 output "nic" {
   description = "The created NIC as an object with all of it's attributes. This was created using the azurerm_network_interface resource."
   value       = azurerm_network_interface.nic
-}
+} */
