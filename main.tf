98abe9378bfb9b45ca43416ea07c181bf17fc430
Frey Khademi <frey.khademi@gmail.com>
2021-02-09 14:29:29 +0100

    fix port

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "azurerm_network_security_group" "nsg" {
     access                     = "Allow"
     protocol                   = "TCP"
     source_port_range          = "*"
-    destination_port_range     = "8080"
+    destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
